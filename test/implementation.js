eebd1e033906d0b741a60a784c467e5769b85473
Jordan Harband <ljharb@gmail.com>
2021-10-01 10:37:17 -0700

    [Dev Deps] use `call-bind` instead of `function-bind`

--- test/implementation.js
+++ test/implementation.js
@@ -2,14 +2,13 @@
 
 var indexOf = require('../implementation');
 var callBind = require('call-bind');
-var bind = require('function-bind');
 var test = require('tape');
 var runTests = require('./tests');
 
 test('as a function', function (t) {
 	t.test('bad array/this value', function (st) {
-		st['throws'](bind.call(indexOf, null, undefined, 'a'), TypeError, 'undefined is not an object');
-		st['throws'](bind.call(indexOf, null, null, 'a'), TypeError, 'null is not an object');
+		st['throws'](callBind(indexOf, null, undefined, 'a'), TypeError, 'undefined is not an object');
+		st['throws'](callBind(indexOf, null, null, 'a'), TypeError, 'null is not an object');
 		st.end();
 	});
 
--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var indexOf = require('..');
-var bind = require('function-bind');
+var callBind = require('call-bind');
 var test = require('tape');
 var runTests = require('./tests');
 
 test('as a function', function (t) {
 	t.test('bad array/this value', function (st) {
-		st['throws'](bind.call(indexOf, null, undefined, 'a'), TypeError, 'undefined is not an object');
-		st['throws'](bind.call(indexOf, null, null, 'a'), TypeError, 'null is not an object');
+		st['throws'](callBind(indexOf, null, undefined, 'a'), TypeError, 'undefined is not an object');
+		st['throws'](callBind(indexOf, null, null, 'a'), TypeError, 'null is not an object');
 		st.end();
 	});
 
