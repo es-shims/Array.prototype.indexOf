7c38d022df705b3782306921a354438da43139ca
Jordan Harband <ljharb@gmail.com>
2023-09-05 13:54:02 -0700

    [Deps] update `define-properties`, `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var Get = require('es-abstract/2022/Get');
-var HasProperty = require('es-abstract/2022/HasProperty');
-var LengthOfArrayLike = require('es-abstract/2022/LengthOfArrayLike');
-var ToIntegerOrInfinity = require('es-abstract/2022/ToIntegerOrInfinity');
-var ToObject = require('es-abstract/2022/ToObject');
-var ToString = require('es-abstract/2022/ToString');
+var Get = require('es-abstract/2023/Get');
+var HasProperty = require('es-abstract/2023/HasProperty');
+var LengthOfArrayLike = require('es-abstract/2023/LengthOfArrayLike');
+var ToIntegerOrInfinity = require('es-abstract/2023/ToIntegerOrInfinity');
+var ToObject = require('es-abstract/2023/ToObject');
+var ToString = require('es-abstract/2023/ToString');
+
 var callBound = require('call-bind/callBound');
 var isNegativeZero = require('is-negative-zero');
 var isString = require('is-string');
